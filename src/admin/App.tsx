46080dda627cd507e7d1e8f050f8d0993e84e6c4
Jamerson Walderson <jamersonwalderson@gmail.com>
2023-02-03 09:21:28 -0300

    feat: criando componente commentlist

--- src/admin/App.tsx
+++ src/admin/App.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { CommentList } from "./comment";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="posts" list={ListGuesser} />
-    <Resource name="comments" list={ListGuesser} />
+    <Resource name="comments" list={CommentList} />
   </Admin>
 );
 
