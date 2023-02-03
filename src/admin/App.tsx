c2346420ad0847ac48dad08a468275890777b36c
Jamerson Walderson <jamersonwalderson@gmail.com>
2023-02-03 16:31:03 -0300

    feat: post create

--- src/admin/App.tsx
+++ src/admin/App.tsx
@@ -2,12 +2,13 @@ import * as React from "react";
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { CommentList } from "./comment";
+import { PostCreate, PostList } from "./post";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={ListGuesser} />
+    <Resource name="posts" list={PostList} create={PostCreate} />
     <Resource name="comments" list={CommentList} />
   </Admin>
 );
---
+++ src/admin/post/index.tsx
@@ -0,0 +1,22 @@
+import { Create, Datagrid, List, ReferenceField, SimpleForm, TextField, TextInput } from 'react-admin';
+
+export const PostList = () => (
+    <List>
+        <Datagrid rowClick="edit">
+            <ReferenceField source="userId" reference="users" />
+            <TextField source="id" />
+            <TextField source="title" />
+            <TextField source="body" />
+        </Datagrid>
+    </List>
+);
+
+export const PostCreate = () => (
+    <Create>
+        <SimpleForm>
+            <TextInput source="id" disabled />
+            <TextInput source="title" />
+            <TextInput source="body" />
+        </SimpleForm>
+    </Create>
+);
